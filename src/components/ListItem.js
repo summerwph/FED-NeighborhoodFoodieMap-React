1d790bd7c50250a53b0cc5feeb0cf738d715fcad
Pei-Hwa Wu <summer.wph@gmail.com>
2018-10-17 17:24:00 -0700

    Fix listItem aria-label undefine issue

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class ListItem extends Component {
   render() {
     return (
-        <li className="listItem" tabIndex = '3' aria-label={"Type"+ this.props.categories[0].icon.name+"Name:"+this.props.name}
+        <li className="listItem" tabIndex = '3' aria-label={"Type: "+ this.props.categories[0].name+" Name: "+this.props.name}
             onClick={() => this.props.handleListItemClick(this.props)}>
             <img src={this.props.categories[0].icon.prefix+"32"+this.props.categories[0].icon.suffix} alt={this.props.categories[0].name} />
             {this.props.name}
